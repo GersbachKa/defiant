Added legendre polynomials. Fixed some PC MCOS normalization term

defiant/orf_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import legendre
 
 from .custom_exceptions import ORFNotFoundError
 
@@ -68,6 +69,7 @@ def get_orf_function(orf_name='hd'):
         - 'gwdp' or 'gw_dipole': Gravitational wave dipole
         - 'gwmp' or 'gw_monopole': Gravitational wave monopole
         - 'st' or 'scalar_tensor': Scalar tensor
+        - 'l_n' or 'legendre_n': Legendre polynomial (where n is the legendre order)
 
     Raises:
         ORFNotFoundError: If the orf_name is not found in the defined_orfs list
@@ -86,6 +88,14 @@ def get_orf_function(orf_name='hd'):
     for orf in defined_orfs: 
         if orf_name.lower() in orf[0]:
             return orf[1]
+        
+        # Legendre polynomials are a bit special
+        if (orf[0][0] in orf_name.lower()) or (orf[0][1] in orf_name.lower()):
+            # This is the legendre polynomial, but which order? It should be 
+            # supplied by the user as 'l_n' or 'legendre_n' where n is the order.
+            l_order = int(orf_name.split('_')[-1])
+            legendre = lambda arg1,arg2=None: orf_legendre(arg1, arg2, l=l_order)
+            return legendre
         
     # If the function is not found, raise an error
     raise ORFNotFoundError(f"The ORF function '{orf_name}' is not found in the defined_orfs list.")
@@ -329,6 +339,40 @@ def orf_st(arg1, arg2 = None):
     return ret
 
 
+def orf_legendre(arg1, arg2 = None, l=0):
+    """A function for legendre polynomial overlap reduction functions
+
+    This function will return the legendre polynomial of order l as an overlap 
+    reduction function using either two enterprise.Pulsar objects or a pulsar 
+    separation in radians. This function supports vectorization.
+
+    Args:
+        arg1 (enterprise.Pulsar or float): The first pulsar object or the pulsar 
+                separation in radians.
+        arg2 (enterprise.Pulsar or None): The second pulsar object or None if arg1 
+                is pulsar separation. Defaults to None.
+
+    Returns:
+        float: The ORF value
+    """
+    if arg2 is not None:
+        # Two pulsar arguments
+        xi = get_pulsar_separation(arg1, arg2)
+    else:
+        # One separation argument
+        xi = arg1
+    
+    ret = legendre(l)(np.cos(xi))
+
+    # Check if the separation is zero (same pulsar)
+    if hasattr(xi, '__iter__'):
+        ret[xi == 0] = 1
+    else:
+        if xi == 0:
+            ret = 1
+    return ret
+
+
 # Anisotropic ORF functions ---------------------------------------------------
 
 # TODO: put in per-telescope orf functions
@@ -337,10 +381,11 @@ def orf_st(arg1, arg2 = None):
 
 
 defined_orfs = [
-    (['hellingsdowns','hd'],    orf_hd),    # Hellings and Downs
-    (['dipole','dp'],           orf_dp),    # Dipole
-    (['monopole','mp'],         orf_mp),    # Monopole
-    (['gw_dipole','gwdp'],      orf_gwdp),  # Gravitational wave dipole
-    (['gw_monopole','gwmp'],    orf_gwmp),  # Gravitational wave monopole
-    (['scalar_tensor','st'],    orf_st),    # Scalar tensor
+    (['hellingsdowns','hd'],    orf_hd),        # Hellings and Downs
+    (['dipole','dp'],           orf_dp),        # Dipole
+    (['monopole','mp'],         orf_mp),        # Monopole
+    (['gw_dipole','gwdp'],      orf_gwdp),      # Gravitational wave dipole
+    (['gw_monopole','gwmp'],    orf_gwmp),      # Gravitational wave monopole
+    (['scalar_tensor','st'],    orf_st),        # Scalar tensor
+    (['legendre_','l_'],        orf_legendre)   # Legendre polynomial
 ]

defiant/pair_covariance.py
@@ -73,7 +73,10 @@ def _compute_mcos_pair_covariance(Z, phi1, phi2, orf, design,
     # Need to compute the a no-PC MCOS for amp estimates
     temp_c = np.square(sig_ab)
     mcos,_ = linear_solve(design,temp_c,rho_ab,'diagonal')
-    est_pow = a2_est*(mcos/np.sum(mcos))
+
+    # Normalize the power per process
+    norm_pow = mcos/np.sqrt(np.dot(mcos,mcos))
+    est_pow = a2_est*norm_pow
 
     # Hijack the factored code by giving it correlated power in ORF and A2=1!
     cor_pow = np.sum([o*a for o,a in zip(orf,est_pow)], axis=0)

defiant/utils.py
@@ -390,7 +390,7 @@ def binned_pair_correlations(xi, rho, sig, bins=10, orf='hd'):
 
 
 def calculate_mean_sigma_for_MCOS(xi, A2, A2_cov, orfs=['hd','dipole','monopole'], 
-                                  n_samples=1000):
+                                  n_samples=1000, clip_thresh=1e-10):
     """Calculate the mean and sigma of the total MCOS fit for a given xi.
 
     For a given pulsar pair separation, xi (can be a vector of xi), this function
@@ -411,11 +411,13 @@ def calculate_mean_sigma_for_MCOS(xi, A2, A2_cov, orfs=['hd','dipole','monopole'
         A2_cov (numpy.ndarray): A covariance matrix between A^2 values
         orfs (list, optional): A list of ORFs to use (either names or custom functions).
         n_samples (int): The number of samples of the fit to generate. Defaults to 1000.
+        clip_thresh (float): The threshold to clip the covariance matrix if needed. Defaults to 1e-10.
     
     Returns:
         (np.ndarray,np.ndarray): The corresponding mean and 1-sigma standard deviation.
     """
-    norm = multivariate_normal(mean=A2,cov=A2_cov,allow_singular=True)
+    clipped = clip_covariance(A2_cov,clip_thresh)
+    norm = multivariate_normal(mean=A2,cov=clipped,allow_singular=True)
     rvs = norm.rvs(size=n_samples)
 
     orf_mods = [get_orf_function(o)(xi) for o in orfs]
@@ -513,7 +515,7 @@ def uncertainty_sample(A2,A2s,pfos=False,mcos=False,n_usamples=100):
     return all_A2
 
             
-def clip_covariance(cov, eig_thresh=1e-15):
+def clip_covariance(cov, eig_thresh=1e-10):
     """A function to clip the small or negative eigenvalues of a covariance matrix
 
     This function is to fix some minor numerical problems with some covariance matrices
@@ -562,11 +564,13 @@ def chi_square(rho, sig, design_mat, a2):
         a2 = a2[:,None]
     if design_mat.ndim == 1:
         design_mat = design_mat[:,None]
+    if rho.ndim == 1:
+        rho = rho[:,None]
     
     model = design_mat @ a2
     residuals = (rho - model)
 
     chi2 = residuals.T @ sig @ residuals
-    return chi2
+    return chi2.item()
 
 